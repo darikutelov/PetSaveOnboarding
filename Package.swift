129f4f46c8f30ab1f9f63bafab89d42fd18ac7c9
darikutelov <dari@kutelov.com>
2024-05-07 21:16:44 +0300

    package.swift updated

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    // 2
     name: "PetSaveOnboarding",
-    // 3
-    platforms: [.iOS(.v17), .macOS(.v10_15)],
-    // 4
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
     products: [
         .library(
             name: "PetSaveOnboarding",
             targets: ["PetSaveOnboarding"]),
     ],
-    // 5
     dependencies: [],
-    // 6
     targets: [
         .target(
-            name: "PetSaveOnboarding",
-            resources: [.copy("Resources/Assets.xcassets")]),
+            name: "PetSaveOnboarding", resources: [.copy("Resources/Assets.xcassets")])
     ]
 )
